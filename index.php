d71471f69294a6dbfec4c9d7ceeefaef9976b6c6
noaritomik <noar.mikullovci@gmail.com>
2025-06-14 14:30:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+$num = 4;
+$Num = 5;
+$_TEst = 7;
+$_test = 3;
+// $1a=3;
+
+$school = "Digital School";
+
+echo "I love $school <br>";
+
+var_dump($school);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$array = ["e1", "e2", "e3"];
+$array2 = array("e1", "e2", "e3");
+
+$test = false;
+
+
+$x=120;
+$y=50;
+
+echo $x + $y . "<br>";
+echo $x - $y . "<br>";
+echo $x * $y . "<br>";
+echo $x / $y . "<br>";
+echo $x % $y . "<br>";
+
+
+$a = "Digital ";
+$b = "School";
+
+$c = $a . $b;
+echo $c;
+echo "<br>";
+$the_string = "Digital School";
+echo strlen($the_string);
+
+echo "<br>";
+
+echo str_word_count($the_string);
+
+echo "<br>";
+echo "<br>";
+
+$programming = "Programming is not cool";
+echo str_replace("not", "very", $programming);
+
+echo "<br>";
+
+echo strrev($programming);
